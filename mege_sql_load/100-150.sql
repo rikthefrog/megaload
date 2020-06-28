486e5b67d65026c53e1cd28533c63626acd313e8
Richard Brouwer <rikthefrog>
2020-06-28 21:46:29 +0200

    update some files

---
+++ mege_sql_load/100-150.sql
@@ -0,0 +1,613 @@
+DATABASE Adventureworksdw ;
+CREATE TABLE FactProductInventory_BlowUp100 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp101 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp102 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp103 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp104 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp105 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp106 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp107 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp108 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp109 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp110 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp111 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp112 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp113 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp114 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp115 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp116 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp117 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp118 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp119 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp120 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp121 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp122 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp123 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp124 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp125 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp126 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp127 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp128 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp129 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp130 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp131 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp132 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp133 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp134 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp135 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp136 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp137 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp138 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp139 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp140 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp141 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp142 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp143 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp144 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp145 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp146 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp147 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp148 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp149 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
+CREATE TABLE FactProductInventory_BlowUp150 AS (SELECT  A1.ColId,
+	        B1.ProductKey,
+        B1.DateKey,
+        B1.MovementDate,
+        B1.UnitCost,
+        B1.UnitsIn,
+        B1.UnitsOut,
+        B1.UnitsBalance
+        FROM FactProductInventory B1
+        CROSS JOIN 
+        CrossJoinTable A1)
+     WITH DATA;
